7015bc5d1775d81c3bd7aa98f5cccd3020f2fbc5
Victor Ibanichuka <94909597+VIbanichuka@users.noreply.github.com>
2024-03-01 21:36:26 +0000

    Replaced the angular project with another one

---
+++ src/ChitChat.App/chitchat.app.client/src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { MatSidenavModule } from '@angular/material/sidenav';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    MatSidenavModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule,
+    NoopAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
