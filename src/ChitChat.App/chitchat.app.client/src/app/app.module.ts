a66895dfacf65f5202cb582f0e430bdaa3c77e34
Victor Ibanichuka <94909597+VIbanichuka@users.noreply.github.com>
2024-03-02 00:01:30 +0000

    Create sidenav bar

--- src/ChitChat.App/chitchat.app.client/src/app/app.module.ts
+++ src/ChitChat.App/chitchat.app.client/src/app/app.module.ts
@@ -2,25 +2,40 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { DmsComponent } from './dms/dms.component';
+import { FriendsComponent } from './friends/friends.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    DmsComponent,
+    FriendsComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     MatSidenavModule,
     FormsModule,
+    MatListModule,
+    MatIconModule,
+    MatButtonModule,
     ReactiveFormsModule,
-    RouterModule,
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent, pathMatch: 'full' },
+      { path: 'home', component: HomeComponent},
+      { path: 'dms', component: DmsComponent},
+      { path: 'friends', component: FriendsComponent},
+    ]),
     NoopAnimationsModule
   ],
   providers: [],
