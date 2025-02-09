c83a0fe597a74d93a2eff21c6a7a8711d9a6c218
Victor Ibanichuka <94909597+VIbanichuka@users.noreply.github.com>
2025-02-09 00:31:07 +0000

    Added API Endpoint for changing password

--- src/ChitChat.App/ChitChat.App.Server/Controllers/AuthController.cs
+++ src/ChitChat.App/ChitChat.App.Server/Controllers/AuthController.cs
@@ -1,4 +1,5 @@
-﻿using ChitChat.App.Server.Models.Requests;
+﻿using System.Security.Claims;
+using ChitChat.App.Server.Models.Requests;
 using ChitChat.Application.Interfaces.IServices;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -23,6 +24,51 @@ namespace ChitChat.App.Server.Controllers
             _externalAuthService = externalAuthService;
         }
 
+        [ProducesResponseType(400)]
+        [ProducesResponseType(401)]
+        [ProducesResponseType(500)]
+        [ProducesResponseType(200)]
+        [HttpPost("change-password")]
+        public async Task<IActionResult> ChangePassword([FromBody] ChangePasswordRequestModel changePasswordRequest)
+        {
+            if(string.IsNullOrWhiteSpace(changePasswordRequest.CurrentPassword) || string.IsNullOrWhiteSpace(changePasswordRequest.NewPassword))
+            {
+                Log.Warning("Invalid user request. Current password and new password are needed.");
+                return BadRequest("Invalid user request");
+            }
+
+            var userIdString = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+            if (!Guid.TryParse(userIdString, out Guid userId))
+            {
+                return Unauthorized("Invalid user Id.");
+            }
+
+            var user = await _userService.GetUserByIdAsync(userId);
+
+            if (user == null)
+            {
+                return Unauthorized("User not found.");
+            }
+
+            var isPasswordVerified = _passwordService.VerifyPasswordHash(changePasswordRequest.CurrentPassword, user.PasswordHash, user.PasswordSalt);
+            
+            if (!isPasswordVerified)
+            {
+                return Unauthorized("Incorrect password");
+            }
+
+            _passwordService.CreatePasswordHash(changePasswordRequest.NewPassword, out byte[] passwordHash, out byte[] passwordSalt);
+
+            user.PasswordHash = passwordHash;
+            user.PasswordSalt = passwordSalt;
+            
+            await _userService.UpdateUserAsync(user);
+
+            return Ok();
+        }
+
+
         [HttpPost("Login")]
         [ProducesResponseType(400)]
         [ProducesResponseType(401)]
@@ -33,6 +79,7 @@ namespace ChitChat.App.Server.Controllers
             if (string.IsNullOrWhiteSpace(userLoginRequest.Email) || string.IsNullOrWhiteSpace(userLoginRequest.Password))
             {
                 Log.Warning("Invalid user login request. Email or password is empty.");
+                
                 return BadRequest("Invalid user");
             }
 
@@ -40,7 +87,7 @@ namespace ChitChat.App.Server.Controllers
 
             if (user == null)
             {
-                return NotFound("Invalid Authentication");
+                return Unauthorized("Invalid Email or Password");
             }
 
             var isPasswordVerified = _passwordService.VerifyPasswordHash(userLoginRequest.Password, user.PasswordHash, user.PasswordSalt);
---
+++ src/ChitChat.App/ChitChat.App.Server/Models/Requests/ChangePasswordRequestModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ChitChat.App.Server.Models.Requests
+{
+    public class ChangePasswordRequestModel
+    {
+
+        [Required, MinLength(6)]
+        public string CurrentPassword { get; set; }
+
+        [Required, MinLength(6)]
+        public string NewPassword { get; set; }
+
+        [Required, Compare("NewPassword")]
+        public string ConfirmPassword { get; set; }
+    }
+}
